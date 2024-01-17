a8f42ea971603e1eb62f99a279c84c7dd8f86c7f
vibin-eng <khareanushka2015@gmail.com>
2024-01-17 12:54:30 -0700

    Adding Devices Screen

--- HIFA-App/app/src/main/java/com/example/hifa/MainActivity.java
+++ HIFA-App/app/src/main/java/com/example/hifa/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.hifa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button datePickerButton;
+    TextView date;
+    DatePickerDialog datePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datePickerButton = findViewById(R.id.datePickerButton);
+        date = findViewById(R.id.datePickerTextView);
+        datePickerButton.setOnClickListener(view -> {
+            datePickerDialog = new DatePickerDialog(MainActivity.this,
+                    (datePicker,))
+        });
     }
 }
