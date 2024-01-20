a57aa64ca346a95f081226d7f8dcd47328cde7e7
Karan <ks2@ualberta.ca>
2024-01-20 13:20:42 -0700

    Home page and bottom nav bar done

--- HIFA-App/app/src/main/java/com/example/hifa/MainActivity.java
+++ HIFA-App/app/src/main/java/com/example/hifa/MainActivity.java
@@ -1,25 +1,60 @@
 package com.example.hifa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.hifa.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    Button datePickerButton;
-    TextView date;
-    DatePickerDialog datePickerDialog;
+//    Button datePickerButton;
+//    TextView date;
+//    DatePickerDialog datePickerDialog;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        datePickerButton = findViewById(R.id.datePickerButton);
-        date = findViewById(R.id.datePickerTextView);
-        datePickerButton.setOnClickListener(view -> {
-            datePickerDialog = new DatePickerDialog(MainActivity.this,
-                    (datePicker,))
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.action_home){
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.action_profile){
+                replaceFragment(new ProfileFragment());
+            }
+            else if (item.getItemId() == R.id.action_settings){
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
         });
+
+//        datePickerButton = findViewById(R.id.datePickerButton);
+//        date = findViewById(R.id.datePickerTextView);
+//        datePickerButton.setOnClickListener(view -> {
+//            datePickerDialog = new DatePickerDialog(MainActivity.this,
+//                    (datePicker,))
+//        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
