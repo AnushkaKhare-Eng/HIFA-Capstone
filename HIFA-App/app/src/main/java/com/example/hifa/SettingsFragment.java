3de59ff3966a7dba6fb0d755cf35245b0ed1e7cb
Karan <ks2@ualberta.ca>
2024-02-04 14:33:25 -0700

    update from a long time

--- HIFA-App/app/src/main/java/com/example/hifa/SettingsFragment.java
+++ HIFA-App/app/src/main/java/com/example/hifa/SettingsFragment.java
@@ -3,10 +3,15 @@ package com.example.hifa;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +22,6 @@ public class SettingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -40,8 +39,6 @@ public class SettingsFragment extends Fragment {
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +46,27 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.subsettings_recyclerview);
+
+        List<SubSettingItem> items = new ArrayList<SubSettingItem>();
+        items.add(new SubSettingItem("Account", R.drawable.user));
+        items.add(new SubSettingItem("Medical Profile", R.drawable.medicalhistory));
+        items.add(new SubSettingItem("Notifications", R.drawable.notification));
+        items.add(new SubSettingItem("Emergency Contacts", R.drawable.emergencycontact));
+        items.add(new SubSettingItem("About", R.drawable.information));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(new SubSettingAdapter(this.getContext(), items));
+
+        return view;
     }
+
 }
