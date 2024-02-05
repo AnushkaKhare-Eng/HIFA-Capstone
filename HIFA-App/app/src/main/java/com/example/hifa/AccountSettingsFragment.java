0c90fe61679cfa4e196a2eddb81bf7d002b90249
vibin-eng <khareanushka2015@gmail.com>
2024-02-04 17:32:20 -0700

    fixing the AccountSettingsFragment

--- HIFA-App/app/src/main/java/com/example/hifa/AccountSettingsFragment.java
+++ HIFA-App/app/src/main/java/com/example/hifa/AccountSettingsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link notificationpage2#newInstance} factory method to
+ * Use the {@link AccountSettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class notificationpage2 extends Fragment {
+public class AccountSettingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class notificationpage2 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public notificationpage2() {
+    public AccountSettingsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class notificationpage2 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment notificationpage2.
+     * @return A new instance of fragment AccountSettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static notificationpage2 newInstance(String param1, String param2) {
-        notificationpage2 fragment = new notificationpage2();
+    public static AccountSettingsFragment newInstance(String param1, String param2) {
+        AccountSettingsFragment fragment = new AccountSettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class notificationpage2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notificationpage2, container, false);
+        return inflater.inflate(R.layout.fragment_account_settings, container, false);
     }
 }
--- HIFA-App/app/src/main/java/com/example/hifa/Notification_page.java
+++ HIFA-App/app/src/main/java/com/example/hifa/Notification_page.java
@@ -62,10 +62,5 @@ public class Notification_page extends Fragment {
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification_page2, container, false);
-    }
+
 }
