36eba81b93c5840a45b4dd670e0e2440d244fe9c
Karan <ks2@ualberta.ca>
2024-02-07 15:28:46 -0700

    Need to rebase

--- HIFA-App/app/src/main/java/com/example/hifa/HomeActivity.java
+++ HIFA-App/app/src/main/java/com/example/hifa/HomeActivity.java
@@ -1,26 +1,15 @@
 package com.example.hifa;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.hifa.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-//    Button datePickerButton;
-//    TextView date;
-//    DatePickerDialog datePickerDialog;
+public class HomeActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-
-//        datePickerButton = findViewById(R.id.datePickerButton);
-//        date = findViewById(R.id.datePickerTextView);
-//        datePickerButton.setOnClickListener(view -> {
-//            datePickerDialog = new DatePickerDialog(MainActivity.this,
-//                    (datePicker,))
-//        });
     }
 
     private void replaceFragment(Fragment fragment){
