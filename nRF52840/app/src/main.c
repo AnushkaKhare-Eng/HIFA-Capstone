c548e842a7e084f8e26ca18aec1c6ce420c65bad
stephuofa <71566670+stephuofa@users.noreply.github.com>
2024-03-08 16:27:49 -0700

    wip saadc using zephyr timer :) much tidying needed

--- nRF52840/app/src/main.c
+++ nRF52840/app/src/main.c
@@ -35,24 +35,54 @@ static void event_handler(nrfx_saadc_evt_t const * p_event)
         {
             p_event->data.done.p_buffer[i];
         }
-
-        is_ready = true;
-    }
-    if (p_event->type == NRFX_SAADC_EVT_READY)
-    if (p_event->type == NRFX_SAADC_EVT_LIMIT)
-    {
-        // low battery light
-        limflag = true;
+        set_led_on(LED_INT_GREEN);
     }
 }
 
+// void saadc_config(){
+//     int err_code = nrfx_saadc_init(7);
+    
+
+// 	#if defined(__ZEPHYR__)
+//         IRQ_DIRECT_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_SAADC), IRQ_PRIO_LOWEST, nrfx_saadc_irq_handler, 0);
+//     #endif
+
+ 
+//     channels[0].channel_config.gain = NRF_SAADC_GAIN1_6;
+    
+    
+//     err_code = nrfx_saadc_channels_config(channels, SAADC_CHANNEL_COUNT);
+    
+
+//     const nrfx_saadc_adv_config_t saadc_adv_config =  {.oversampling = NRF_SAADC_OVERSAMPLE_DISABLED,
+//                                                        .burst = NRF_SAADC_BURST_DISABLED,
+//                                                        .internal_timer_cc = 0,
+//                                                        .start_on_end=true};
+
+// 	err_code = nrfx_saadc_simple_mode_set((1<<0),
+// 											NRF_SAADC_RESOLUTION_12BIT,
+// 											NRF_SAADC_OVERSAMPLE_DISABLED,
+// 											event_handler);
+
+	
+// 	err_code = nrfx_saadc_buffer_set(samples, SAADC_CHANNEL_COUNT);
+
+
+// 	err_code = nrfx_saadc_mode_trigger();
+// }
+
+void my_timer_handler(struct k_timer *dummy)
+{
+    set_led_on(LED_INT_RGB_BLUE);
+    int err_code = nrfx_saadc_mode_trigger();
+}
  
 int main(void)
 {
     int err_code;
 
-
-    
+    err_code = leds_init();
+    //saadc_config();
     err_code = nrfx_saadc_init(7);
     
 
@@ -67,35 +97,32 @@ int main(void)
     err_code = nrfx_saadc_channels_config(channels, SAADC_CHANNEL_COUNT);
     
 
-    const nrfx_saadc_adv_config_t saadc_adv_config =  {.oversampling = NRF_SAADC_OVERSAMPLE_DISABLED,
-                                                       .burst = NRF_SAADC_BURST_DISABLED,
-                                                       .internal_timer_cc = 1,
-                                                       .start_on_end=true};
+    // const nrfx_saadc_adv_config_t saadc_adv_config =  {.oversampling = NRF_SAADC_OVERSAMPLE_DISABLED,
+    //                                                    .burst = NRF_SAADC_BURST_DISABLED,
+    //                                                    .internal_timer_cc = 0,
+    //                                                    .start_on_end=true};
 
-	err_code = nrfx_saadc_advanced_mode_set((1<<0),
+    err_code = nrfx_saadc_simple_mode_set((1<<0),
 											NRF_SAADC_RESOLUTION_12BIT,
-											&saadc_adv_config,
+											NRF_SAADC_OVERSAMPLE_DISABLED,
 											event_handler);
-
-    // this must be set after seting the mode, any change to mode will reset the limits
-    // Only non-blocking conversions can be monitored -> 
-    err_code = nrfx_saadc_limits_set(VDD_CHANNEL, LIM_L, LIM_H);
 
 	
 	err_code = nrfx_saadc_buffer_set(samples, SAADC_CHANNEL_COUNT);
 
-
-	err_code = nrfx_saadc_mode_trigger();
-
-   
+    struct k_timer my_timer;
+    k_timer_init (&my_timer,my_timer_handler, NULL);
+	k_timer_start(&my_timer, K_SECONDS(5), K_SECONDS(5));
 
 
 
 	while(1){
-		k_sleep(K_MSEC(100));
-		if(is_ready){
-			is_ready = false;
-		}
+        set_led_on(LED_INT_RGB_RED);
+		k_sleep(K_MSEC(1000));
+        set_led_off(LED_INT_RGB_RED);
+        set_led_off(LED_INT_RGB_BLUE);
+        set_led_off(LED_INT_GREEN);
+        k_sleep(K_MSEC(1000));
 	}
     
 }
